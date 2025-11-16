5d0b497fff78ae7ecf3db109312cd0cb1c79c610
kjsckp <kjscikampek@gmail.com>
2025-11-17 05:01:12 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  TARGET_GROUP_NAME: "KJS Ai",
-  DIFYAI_API_URL: process.env.DIFYAI_API_URL || "https://api.dify.ai/v1/chat-messages",
-  DIFYAI_API_KEY: process.env.DIFYAI_API_KEY || "app-f7dMo1mAYcaEX9H7tR7cFr09",
-  BOT_NAME: "KJS Ai"
-};
